4b9e5380683571e935f8d724dd14329c02137c08
Naim Shaqqou <Naimshaqqou@knights.ucf.edu>
2023-04-09 17:52:51 -0400

    implement add/remove correctly

--- problem1.cpp
+++ problem1.cpp
@@ -12,7 +12,6 @@ struct gift
 
 struct node
 {
-    int val;
     int tag;
     
     // this locks the node pointed to
@@ -26,63 +25,209 @@ struct node
 
 struct linked_list
 {
-    node *head, *tail;
-    int size;
+    node *head = nullptr;
+    int size = 0;
 };
 
-linked_list *ll;
+linked_list *ll = new linked_list;
 
 bool validate(node *prev, node *cur)
 {
+    if (cur == nullptr)
+        return !prev->marked && prev->next == cur;
+
     return !prev->marked && !cur->marked && prev->next == cur;
+}
+
+bool add(gift item)
+{
+    int tag = item.tag;
+    cout << "gift tag = " << tag << "\n";
+
+    while (true)
+    {
+        if (ll->size == 0)
+        {
+            // create node and add to the list
+            node *newNode = new node;
+            newNode->tag = tag;
+
+            ll->head = newNode;
+            ll->head->next = nullptr;
+            ll->size++;
+
+            return true;
+        }
+
+        if (ll->size == 1)
+        {
+            node *newNode = new node;
+            newNode->tag = tag;
+
+            if (ll->head->tag > tag)
+            {
+                newNode->next = ll->head;
+                ll->head = newNode;
+            }
+            else
+            {
+                ll->head->next = newNode;
+                newNode->next = nullptr;
+            }
+
+            ll->size++;
+            return true;
+        }
+
+
+        node *prev = ll->head;
+        node *cur = ll->head->next;
+        while (cur != nullptr && cur->tag < tag)
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+
+        cout << "found place to add to:\n" << "prev: " << prev->tag << "\n";
+
+        // lock both pred and cur nodes
+        prev->next_mutex.lock();
+
+        if (cur != nullptr)
+            cur->next_mutex.lock();
+
+        cout << "acquired locks\n";
+
+        if (validate(prev, cur))
+        {
+            cout << "validated\n";
+
+            // if the gift is already in the list
+            if (cur != nullptr && cur->tag == tag)
+            {
+                prev->next_mutex.unlock();
+                cur->next_mutex.unlock();
+                return false;
+            }
+            // create node and add to the list
+            node *newNode = new node;
+            newNode->tag = tag;
+
+            newNode->next = cur;
+            prev->next = newNode;
+
+            ll->size++;
+            cout << "added node\n";
+
+            // unlock both pred and cur nodes
+            prev->next_mutex.unlock();
+            if (cur != nullptr)
+                    cur->next_mutex.unlock();
+            
+            return true;
+        }
+
+    }
 }
 
 bool remove(gift item)
 {
+    if (ll->size == 0) return false;
+
     // get key of current item
     int tag = item.tag;
 
-    // navigate to proper node. Get pred and cur node
-    node *pred = ll->head;
-    node *cur = ll->head->next;
-    while (cur->tag < tag)
+    while (true)
     {
-        pred = cur;
-        cur = cur->next;
-    }
+        // navigate to proper node. Get pred and cur node
+        node *pred = ll->head;
+        node *cur = ll->head->next;
 
-    // lock both pred and cur nodes
-    pred->next_mutex.lock();
-    cur->next_mutex.lock();
-
-    // "validate" the node to delete
-    if (validate(pred, cur))
-    {
-        if (cur->tag == tag)
+        if (pred->tag == tag)
         {
-            // mark the node to remove
-            cur->marked = true;
+            cur = pred;
+            pred = nullptr;
 
-            // physically remove the node
-            pred->next = cur->next;
+            cur->marked = true;
+            ll->head = ll->head->next;
+            return true;
         }
         else
         {
-            return false;
+            while (cur->tag < tag)
+            {
+                pred = cur;
+                cur = cur->next;
+            }
         }
-    }
 
-    // unlcok both pred and cur nodes
-    pred->next_mutex.unlock();
-    cur->next_mutex.unlock();
+        // lock both pred and cur nodes
+        pred->next_mutex.lock();
+        cur->next_mutex.lock();
+
+        // "validate" the node to delete
+        if (validate(pred, cur))
+        {
+            if (cur->tag == tag)
+            {
+                // mark the node to remove
+                cur->marked = true;
+
+                // physically remove the node
+                pred->next = cur->next;
+
+                ll->size--;
+
+                if (ll->size == 0)
+                    ll->head = nullptr;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // unlcok both pred and cur nodes
+        pred->next_mutex.unlock();
+        cur->next_mutex.unlock();
+    }
 }
 
 bool contains(gift item)
 {
-    return false;
+    int tag = item.tag;
+    node *cur = ll->head;
+
+    while(cur->tag < tag)
+    {
+        cur = cur->next;
+    }
+
+    // make sure the tags match and the node is not removed
+    return cur->tag == tag && !cur->marked;
 }
 
 int main()
 {
+    for (int i = 0; i < 5; i++)
+    {
+        gift g;
+        g.tag = i;
+        add(g);
+    }
+
+    gift g;
+    g.tag = 0;
+    remove(g);
+
+    node *temp = ll->head;
+    for (int i = 0; i < ll->size; i++)
+    {
+        cout << temp->tag << "\n";
+        temp = temp->next;
+    }
+
     return 0;
 }
